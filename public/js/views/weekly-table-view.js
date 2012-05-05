943df79e4edb01b648a843fb99bd8c61e5e0abe0
Michael Lam <treadsafely@gmail.com>
2012-05-05 15:59:29 +0100

    hightlight today in calendar

--- public/js/views/weekly-table-view.js
+++ public/js/views/weekly-table-view.js
@@ -8,13 +8,21 @@ define(['jquery', 'underscore', 'backbone', 'router'],
 	        		initialize: function() {
 	        			var today = new Date()
 	        			,	firstJan = new Date(new Date().getFullYear(),0,1)
-	        			,	firstSunday = firstJan.getDay();
+	        			,	firstSunday = firstJan.getDay()
+	        			,	todaysDayElement;
 
 	        			$(".weekly-table-day").each(function(){
 	        				var dataDayValue = $(this).attr('data-day');
 	        				var daysToAdd = parseInt(dataDayValue) + (-1 * firstSunday - 1);
-	        				$(this).attr('data-date', (firstJan.addDays(daysToAdd)));
+	        				var dateOfThisDay = firstJan.addDays(daysToAdd);
+	        				$(this).attr('data-date', dateOfThisDay);
+	        				$(this).children(".date").html(dateFormat(dateOfThisDay, "mmmm d"));
 	        			});
+
+	        			todaysDayElement = $(".weekly-table-day[data-date*='" + dateFormat(today, "mmmm dd") +"']");
+	        			console.log("today's day element", todaysDayElement);
+	        			todaysDayElement.addClass("today");
+
 
 	        		},
 	        		events: {
