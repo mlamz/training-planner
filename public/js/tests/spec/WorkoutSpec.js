082816943d70a686acfc191ee0f49d377c643cd8
Michael Lam <michael@michaellam.co.uk>
2012-05-14 10:09:35 -0600

    first jasmine spec done for workout

--- public/js/tests/spec/WorkoutSpec.js
+++ public/js/tests/spec/WorkoutSpec.js
@@ -1,15 +1,25 @@
-define([], 
-  function() {
+define(['../../models/workout'], 
+  function(Workout) {
     describe("Workout", function() {
 
-      it("should run a test", function() {
+		it("should have /workout as urlRoot", function() {
+			var workout = new Workout();
 
-        expect(true).toEqual(true);
-      });
+			expect(workout.urlRoot).toEqual('/workout');
+		});
 
- 
+		it("should return /workout for url if it is new", function() {
+			var workout = new Workout();
+
+			expect(workout.url()).toEqual('/workout');
+		});
+
+		it("should return /workout/1 for url if it has an id of 1", function() {
+			var workout = new Workout({ _id : 1 });
+
+			expect(workout.url()).toEqual('/workout/1');
+		});
       });
   }
-  
 )
 
