736a3b2157701f4661f207f673ca100fa5acd049
Michael Lam <michael@michaellam.co.uk>
2012-05-14 09:55:32 -0600

    jasmine setup

---
+++ public/js/tests/spec-runner.js
@@ -0,0 +1,10 @@
+define(['spec/workoutSpec'], 
+	function(workoutSpec) {
+		return {
+			initialize: function(){
+				jasmine.getEnv().addReporter(new jasmine.TrivialReporter());
+  				jasmine.getEnv().execute();
+			}
+		}
+	}
+)
---
+++ public/js/tests/spec/WorkoutSpec.js
@@ -0,0 +1,15 @@
+define([], 
+  function() {
+    describe("Workout", function() {
+
+      it("should run a test", function() {
+
+        expect(true).toEqual(true);
+      });
+
+ 
+      });
+  }
+  
+)
+
